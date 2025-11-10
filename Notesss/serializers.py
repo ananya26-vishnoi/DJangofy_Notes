97d59b19dbbe894914f8003de78f616f6deee58c
Ananya Vishnoi <71982267+ananya26-vishnoi@users.noreply.github.com>
2025-11-10 21:58:07 +0530

    commit message

---
+++ Notesss/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers 
+from .models import ABC
+
+class ABCSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = ABC 
+        fields = '__all__' 
